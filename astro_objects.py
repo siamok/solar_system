40439dfaa8e376d42d1001c7c21b4233cf51fd2f
saders <saders@o2.pl>
2016-01-17 01:03:14 +0100

    Blisko konca

--- astro_objects.py
+++ astro_objects.py
@@ -1,7 +1,8 @@
 ﻿# masa w kg!
 
+
 #################################################################################################
-AU = 149597870.7  ## do skalowania jakbym robił GUI albo to wsadze w innym pliku
+AU = 149597870.7  ## do skalowania jakbym robil GUI albo to wsadze w innym pliku
 mercury = {"Name": "Mercury", "Mass": 3.3302 * 10 ** 23, "Radius": 2439.7, "Dist_to_star": 57909176.0}
 venus = {"Name": "Venus", "Mass": 4.8685 * 10 ** 24, "Radius": 6051.85, "Dist_to_star": 108208926.0}
 earth = {"Name": "Earth", "Mass": 5.97219 * 10 ** 24, "Radius": 6378.1366, "Dist_to_star": 149598261.0}
@@ -23,57 +24,58 @@ class Astro_object(object):
         self.mass = mass
 
 
+#################################################################################################
 class Star(Astro_object):
     def __init__(self, star_info):
         Astro_object.__init__(self, star_info["Name"], star_info["Mass"])
         self.color = star_info["Color"]
         self.radius = star_info["Radius"]
 
-    if __name__ == "__main__":
-        def __repr__(self):
-            return repr((self.name, self.mass, self.radius, self.color))
+    def __repr__(self):
+        return repr((self.name, self.mass, self.radius, self.color))
 
-        def __str__(self):
-            return "Name of the star: %s\nMass: %.2ekg\
-        \nRadius: %.2fkm\
-        \nColor of the star:%d\n" % (self.name, self.mass, self.radius, self.color)
+    def __str__(self):
+        return "Name of the star: %s\nMass: %.2ekg\
+    \nRadius: %.2fkm\
+    \nColor of the star:%s\n" % (self.name, self.mass, self.radius, self.color)
 
 
-class Planet(Astro_object):
+#################################################################################################
+class Planets(Astro_object):
     def __init__(self, plnts_dict):
         Astro_object.__init__(self, plnts_dict["Name"], plnts_dict["Mass"])
         self.radius = plnts_dict["Radius"]
         self.dist_to_star = plnts_dict["Dist_to_star"]
-        
-    if __name__ == "__main__":
-        def __repr__(self):
-            return repr({"Name": self.name, "Mass": self.mass, "Radius": self.radius, "Dist_to_star": self.dist_to_star})
 
-        def __str__(self):
-            return "Name of the planet: %s\nMass: %.2ekg\
-        \nRadius: %.2fkm\
-        \nDistance from the star:%fAU\n" % (self.name, self.mass, self.radius, self.dist_to_star / AU)
+    def __repr__(self):
+        return repr({"Name": self.name, "Mass": self.mass, "Radius": self.radius, "Dist_to_star": self.dist_to_star})
+
+    def __str__(self):
+        return "Name of the planet: %s\nMass: %.2ekg\
+    \nRadius: %.2fkm\
+    \nDistance from the star:%fAU\n" % (self.name, self.mass, self.radius, self.dist_to_star / AU)
 
     @classmethod
     def mk_planet(cls, plnts_dict):
-        return Planet.__init__(plnts_dict)
+        return Planets.__init__(plnts_dict)
 
     def __getitem__(self, key):
         if not isinstance(key, str):
-            raise ValueError()
-        if not key.lower() in dir(self):  # czy jest taka obiekt w tej klasie
-            raise ValueError()
+            raise TypeError()
+        if not key.lower() in dir(self):  # czy jest taki obiekt w tej klasie
+            raise KeyError()
         return getattr(self, key.lower())
 
 
+#################################################################################################
 class Star_system:
     planets = []
 
     def __init__(self, name="Solar System", star_info=default_star, names_plnts=default_planets):
         self.name = name
-        self.star = Star(star_info);
+        self.star = Star(star_info)
         for i in range(len(names_plnts)):
-            temp = Planet(names_plnts[i])
+            temp = Planets(names_plnts[i])
             self.planets.append(temp)
 
     def show_planets(self):
@@ -86,21 +88,88 @@ class Star_system:
         else:
             return self.planets[index-1]
 
+    def __del__(self):
+        print "You monster! You have just destroyed your Star System!"
+        del self
+
+    def __repr__(self):
+        return repr(self.name)
+
+    def __str__(self):
+        temp = ""
+        for i in range(len(self.planets)):
+            temp += self.planets[i]["Name"] + "\n"
+        return "Name of the system: %s\nStar: %s\nPlanets:\n%s" % (self.name, self.star.name, temp)
+
+    class Decorator(object):
+        def __init__(self, what):
+            if what == "size":
+                self.what = "Size of the system "
+                self.units = " AU"
+            elif what == "mass":
+                self.what = "System weights "
+                self.units = " kg"
+            elif what == "":
+                pass
+            else:
+                raise TypeError()
+
+        def __call__(self, funk):
+            def _i_(cls):
+                test = funk(cls)
+                print self.what, test, self.units
+                _i_.__name__ = funk.__name__
+                _i_.__doc__ = funk.__doc__
+            return _i_
+
+    def list_of_planets(self):
+        #names = [self.planets[i]["Name"] for i in range(self.planets.__len__())]    # list compr jest czytelniejsze, ale...
+        names = map(lambda x: self.planets[x]["Name"], range(self.planets.__len__()))# <- nice :D trzy pieczenie na jednym ogniu
+        return names
+
+    @Decorator("mass")
+    def mass_of_system(self):
+        mass = 0
+        for j in range(self.planets.__len__()):
+            mass += self.planets[j]["Mass"]
+        return mass
+
+    @Decorator("size")
     def size(self):
-        return self.planets.__len__()
+        return self.planets[-1]["Dist_to_star"]
 
-    if __name__ == "__main__":
-        def __repr__(self):
-            return repr(self.name)
+    ##
+    def max_gen(self):
+        def prize(what):
+            if not isinstance(what, str):
+                raise TypeError()
+            temp = 0
+            for i in range(self.planets.__len__()):
+                if self.planets[i][what] > temp:
+                    try:
+                        temp = self.planets[i][what]
+                    except KeyError:
+                        print "dupa"
+            return temp
+        return prize
 
-        def __str__(self):
-            temp = ""
-            for i in range(len(self.planets)):
-                temp += self.planets[i]["Name"] + "\n"
-            return "Name of the system: %s\nStar: %s\nPlanets:\n%s" % (self.name, self.star.name, temp)
+#################################################################################################
 
-
+#######################################################
 if __name__ == "__main__":
+
     star_system = Star_system()
     print star_system
-    print star_system.size()
+    planet = Planets(mercury)
+    print planet
+    star_system.mass_of_system()
+    star_system.size()
+    print
+    print star_system[0]
+
+    u = star_system.list_of_planets()
+    print u
+    print
+    temp = star_system.max_gen()("Mass")
+    print "%.2ekg" % temp
+    del star_system
